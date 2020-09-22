655f2b5cb3d921a1808712c70baf67fd67b7290a
danny-viper <hextra@live.com>
2020-09-22 18:44:34 +0100

    Inference can now be performed succesfully. Also, flags supported was added with googles fire library.

--- inference.py
+++ inference.py
@@ -1,3 +1,6 @@
+import sys
+import fire
+
 from pathlib import Path
 from data_declaration import DataSample, Task, ToTensor
 from loader_helper    import loader_helper
@@ -5,16 +8,18 @@ from architecture import load_cam_model
 from torchvision import transforms
 import torch
 
-def __main__(str):
-    print("alternative baby")
+device = None
 
-def __main__():
-    device = None
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+else:
+    device = torch.device("cpu")
 
-    if torch.cuda.is_available():
-        device = torch.device("cuda:0")
-    else:
-        device = torch.device("cpu")
+def run(task=-1, weights_dir="../weights/", sample_loc="../data/inference"):
+
+    if (task == -1):
+        task = input("Input the task you want to perform [AD vs CN: 0, sMCI vs pMCI: 1]: ")
+
 
     if (Path('../weights').exists()):
         #task = input("Please specify what task you want to perform [1:\'CN vs AD\', 2:\'sMCI vs pMCI\']: ")
@@ -27,7 +32,7 @@ def __main__():
             ld_helper = loader_helper(task=Task.sMCI_v_pMCI)
 
         #weights_path = input("Please specify the directory of the weights file that you want to use: ") # should be relative to the proj folder and use forward slashes.
-        weights_path = "../weights/CN_v_AD/c51bf83c4455416e8bc8b1ebbc8b75ca/fold_5_weights-2020-04-29_13_17_33"
+        weights_path = "../weights/CN_v_AD/c51bf83c4455416e8bc8b1ebbc8b75ca/fold_5_weights-2020-04-29_13:17:33"
         print("\n")
         print("Loading camull for inference...")
         model = load_cam_model(weights_path)
@@ -50,6 +55,8 @@ def __main__():
 
             with torch.no_grad():
                 outputs = model((mri.view(-1, 1, 110, 110, 110), clinical.view(1, 21)))
+
+                print(outputs)
                 
         except Exception as e:
             print(e)
@@ -57,7 +64,5 @@ def __main__():
         print("You must either place a weights file in the weights folder or train the model by running: python train_model.py")
 
 
-def __cycle__():
-    print("blah")
-
-__main__()
+if __name__ == '__main__':
+    fire.Fire(run)
