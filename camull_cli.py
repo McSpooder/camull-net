92832132ff94b176a81b08c522e54258f7b31e07
danny@remy <hextra@live.com>
2022-06-22 15:20:05 +0100

    A new option added to the main menu and inference for pMCI.

--- camull_cli.py
+++ camull_cli.py
@@ -1,5 +1,4 @@
-from re import M
-from train_model          import DEVICE, start
+from train_model          import start
 from evaluation           import evaluate_fold, evaluate_model
 from data_declaration     import Task
 from loader_helper        import LoaderHelper
@@ -68,7 +67,7 @@ def basic_run(device):
     print("   .  --\|/--  ,  2. Train an existing model or do transfer learning.")
     print("    '--|___|--'   3. Make an inference with an existing model.")
     print("    ,--|___|--,   4. Evaluate a model. ")
-    print("   '  /\o o/\  `    ")
+    print("   '  /\o o/\  `  5. Display the most recent evaluated models.")
     print("     +   +   +    ")
     print("      `     '    ")
     print("\n")
@@ -84,6 +83,9 @@ def basic_run(device):
         make_an_inference(device)
     elif (int(choice) == 4):
         evaluate_a_model(device)
+    elif (int(choice) == 5):
+        fetch_models_from_db(evaluated=True)
+        basic_run(device)
 
 def make_an_inference(device, mri=None, clin=None):
 
@@ -93,67 +95,64 @@ def make_an_inference(device, mri=None, clin=None):
     print("\n")
     choice = int(input("Which task would you like to perform?: "))
     
+    if choice == 1: task = Task.NC_v_AD
+    else: task = Task.sMCI_v_pMCI
 
-    if (choice == 1):
-        #fetch the most recent models for NC vs AD.
-        print("\n")
-        print("Here are the 5 most recent models trained for NC vs AD.")
-        print("    model uuid               | Time      | model task | accuracy | sensitivity | specificity | roc_auc")
-        result = cur.execute("SELECT * FROM nn_perfomance WHERE model_task is 'NC_v_AD' ORDER BY time DESC LIMIT 5")
-        model_uuids = []
-        for i, row in enumerate(result):
-            print(i+1, row)
-            model_uuids.append(row[1])
-        print("\n")
-        choice = int(input("Please enter an index to use [1, 5]: "))
-        path_a = "../weights/{}/{}/*".format(str(Task(1)), model_uuids[choice-1])
-        path = glob.glob(path_a)[0]
-        while (True):
-            if (int(choice) != 6):
-                if torch.is_tensor(mri) == False:
-                    mri, clinical = get_subject_info()
-                    #Performs necessery transformations for the neural network
-                    mri = torch.from_numpy(mri) / 255.0 
-                    mri = mri.unsqueeze(0)
-                    mri = mri.to(device)
-                    clin = torch.from_numpy(clinical)
-                    clin = clin.unsqueeze(0)
-                    clin = clin.to(device)
+    #fetch the most recent models for NC vs AD.
+    print("\n")
+    print("Here are the 5 most recent models trained for {}.".format(str(task)))
+    print("    model uuid               | Time      | model task | accuracy | sensitivity | specificity | roc_auc")
+    result = cur.execute("SELECT * FROM nn_perfomance WHERE model_task is '{}' ORDER BY time DESC LIMIT 5".format(str(task)))
+    model_uuids = []
+    for i, row in enumerate(result):
+        print(i+1, row)
+        model_uuids.append(row[1])
+    print("\n")
+    choice = int(input("Please enter an index to use [1, 5]: "))
+    path_a = "../weights/{}/{}/*".format(str(task), model_uuids[choice-1])
+    path = glob.glob(path_a)[0]
+    while (True):
+        if (int(choice) != 6):
+            if torch.is_tensor(mri) == False:
+                mri, clinical = get_subject_info()
+                #Performs necessery transformations for the neural network
+                mri = torch.from_numpy(mri) / 255.0 
+                mri = mri.unsqueeze(0)
+                mri = mri.to(device)
+                clin = torch.from_numpy(clinical)
+                clin = clin.unsqueeze(0)
+                clin = clin.to(device)
 
-                model = load_cam_model(path)
-                model.eval()
+            model = load_cam_model(path)
+            model.eval()
 
-                net_out = -1
-                with torch.no_grad():
-                    net_out = model((mri.view(-1, 1, 110, 110, 110), clin.view(1, 21)))
+            net_out = -1
+            with torch.no_grad():
+                net_out = model((mri.view(-1, 1, 110, 110, 110), clin.view(1, 21)))
+                if task == Task.NC_v_AD:
                     print("The probability that the subject has AD is " + str(net_out[0].item()*100)  + "%")
-                    print("\n")
-                    print("The probability is deterministic and hinges upon the neural network model and subject data.")
-                    print("\n")
-                    print("1. Input different subject details for the same model.")
-                    print("2. Use the same subject details for inference with another model.")
-                    print("3. Make a new inference with another model.")
-                    print("4. Return to the main menu.")
-                    print("\n")
-                    choice = int(input("Please enter your choice: "))
-                    if choice == 4:
-                        basic_run(device)
-                    elif choice == 3:
-                        make_an_inference(device)
-                    elif choice == 2:
-                        make_an_inference(device, mri, clin)
-                    elif choice == 1:
-                        mri = None
-                        clin = None
-                        continue
-            else:
-                print("Invalid selection.")
-
-    else:
-        #fetch the most recent models for sMCI vs pMCI
-        print("Here are the 5 most recent models trained for sMCI vs pMCI.")
-        print("    model uuid               | Time      | model task | accuracy | sensitivity | specificity | roc_auc")
-        result = cur.execute("SELECT * FROM nn_perfomance WHERE model_task is 'sMCI_v_pMCI' ORDER BY time DESC LIMIT 5")
+                else:
+                    print("The probability that the subject will convert to AD within 3 years is " + str(net_out[0].item()*100)  + "%")
+                print("\n")
+                print("The probability is deterministic and hinges upon the neural network model and subject data.")
+                print("\n")
+                print("1. Input different subject details for the same model.")
+                print("2. Use the same subject details for inference with another model.")
+                print("3. Make a new inference with another model.")
+                print("4. Return to the main menu.")
+                print("\n")
+                choice = int(input("Please enter your choice: "))
+                if choice == 4:
+                    basic_run(device)
+                elif choice == 3:
+                    make_an_inference(device)
+                elif choice == 2:
+                    make_an_inference(device, mri, clin)
+                elif choice == 1:
+                    mri = None
+                    clin = None
+                    continue
+   
 
 def transfer_learning(device):
     print("To train for sMCI vs pMCI you need transfer learning from a NC vs AD model. Would you like to transfer learning from an existing model or train a new NC vs AD model?\n")
@@ -273,6 +272,7 @@ def train_new_model_cli(device):
     else:
         basic_run(device) #loop back round to the start of the program.
 
+
 def evaluate_a_model(device):
     print("Which task would you like to evaluate?")
     print("1) NC v AD")
@@ -287,7 +287,7 @@ def evaluate_a_model(device):
 
     print("Here are 10 of your most recent unevaluated {} models.".format(str(task)))
     print("\n")
-    print("    model uuid               | Time      | model task | accuracy | sensitivity | specificity | roc_auc")
+    print("    id  | model uuid               | Time      | model task")
     model_uuids = fetch_models_from_db(task)
     target_uuid = ""
 
@@ -312,9 +312,9 @@ def evaluate_a_model(device):
         choice = int(input("Enter your choice [1,6]: "))
         if (choice != 6):
             evaluate_fold(device, target_uuid, ld_helper, choice, commit_to_db=True)
-            remove_from_db(target_uuid)
         else:
             evaluate_model(device, target_uuid, ld_helper, cur)
+            remove_from_db(target_uuid)
         
 
     else:
@@ -324,21 +324,24 @@ def evaluate_a_model(device):
         if choice  == 'y' or 'Y' or '': train_new_model_cli 
         else: basic_run
 
-def fetch_models_from_db(task, evaluated=False):
+def fetch_models_from_db(task=None, evaluated=False):
     global conn
     global cur
     model_uuids = []
+
     i = 0
-    for i, row in enumerate(cur.execute("SELECT * FROM nn_basic WHERE model_task is '{}'".format(str(task)))):
+    if task == None: sql_querry = "SELECT * FROM nn_perfomance"
+    else: sql_querry = "SELECT * FROM nn_basic WHERE model_task is '" + str(task) + "'"
+
+    for i, row in enumerate(cur.execute(sql_querry)):
         print(str(i+1) + ". ", row)
         model_uuids.append(row[1])
     return model_uuids
 
 
 def remove_from_db(model_uuid):
-    sql_statement = "DROP * FROM nn_basic WHERE model_uuid is " + model_uuid ################
+    sql_statement = "DELETE FROM nn_basic WHERE model_uuid is '" + model_uuid + "'"
     cur.execute(sql_statement)
-    conn.commit()
 
 
 def get_subject_infor_from_db():
